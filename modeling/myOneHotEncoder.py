8654642cf54ff4a54b04aa66e1e10675d3299684
Galvayra <voltexlicer@gmail.com>
2018-03-16 15:45:20 +0900

    make variables script in modeling

--- modeling/myOneHotEncoder.py
+++ modeling/myOneHotEncoder.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
 import math
+from .variables import *
+
 
 # initial information & Past history 만을 이용하여 학습
-
-scalar_columns = {
-    # "0": ['O']
-    "0": ['D', 'E', 'G', 'I', 'L', 'M', 'N', 'O', 'Q', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC'],
-    "start_1": ['H', 'J'],
-    # "end_1": [''],
-    "start_1_end_1": ['K']
-    }
-
-class_columns = ['P', 'R', 'AD']
-MIN_SCALING = 0.1
-
-
 class MyOneHotEncoder:
     def __init__(self):
         self.vector_dict = dict()
---
+++ modeling/variables.py
@@ -0,0 +1,12 @@
+
+scalar_columns = {
+    # "0": ['O']
+    "0": ['D', 'E', 'G', 'I', 'L', 'M', 'N', 'O', 'Q', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC'],
+    "start_1": ['H', 'J'],
+    # "end_1": [''],
+    "start_1_end_1": ['K']
+    }
+
+class_columns = ['P', 'R', 'AD']
+MIN_SCALING = 0.1
+
