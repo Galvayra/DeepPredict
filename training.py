570d16e28e54768c31ad01ecd402f9fe5125d054
Galvayra <voltexlicer@gmail.com>
2018-04-05 19:49:38 +0900

    latest

--- training.py
+++ training.py
@@ -17,12 +17,9 @@ import DeepPredict.arguments as op
 if __name__ == '__main__':
     csv_name = DATA_FILE.split('.')[0]
 
-    file_name = DUMP_PATH + DUMP_FILE + op.FILE_VECTOR
-
-    try:
-        with open(DUMP_PATH + DUMP_FILE + "_" + file_name, 'r') as file:
-            vector_list = json.load(file)
-    except FileNotFoundError:
+    if op.FILE_VECTOR:
+        file_name = op.FILE_VECTOR
+    else:
         if op.USE_W2V:
             append_name = "w2v_"
         else:
@@ -36,15 +33,15 @@ if __name__ == '__main__':
 
         file_name = append_name + csv_name + "_" + str(op.NUM_FOLDS)
 
-        try:
-            with open(DUMP_PATH + DUMP_FILE + "_" + file_name, 'r') as file:
-                vector_list = json.load(file)
-        except FileNotFoundError:
-            print("\nPlease execute encoding script !")
-            print("make sure whether vector file is existed in", DUMP_PATH, "directory")
+    try:
+        with open(DUMP_PATH + DUMP_FILE + "_" + file_name, 'r') as file:
+            vector_list = json.load(file)
+    except FileNotFoundError:
+        print("\nPlease execute encoding script !")
+        print("make sure whether vector file is existed in", DUMP_PATH, "directory")
+    else:
+        print("\nRead vectors -", file_name)
+        op.show_options()
 
-    print("\nRead vectors -", file_name)
-    op.show_options()
-
-    train = MyTrain(vector_list)
-    train.training(tensor_save=TENSOR_PATH + file_name)
+        train = MyTrain(vector_list)
+        train.training(tensor_save=TENSOR_PATH + file_name)
