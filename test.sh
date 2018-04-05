c7a733bf41588534d641a0e00c906a6cc4075f97
Galvayra <voltexlicer@gmail.com>
2018-04-05 23:59:58 +0900

    modify test.sh

--- test.sh
+++ test.sh
@@ -3,21 +3,21 @@ echo
 echo "h_00_ep_w2v"
 echo
 #python training.py -id origin_scale -w2v 1 -epoch 20000 -hidden 0 > result/reverse_h_00_ep_20000_w2v
-python training.py -train opend -epoch 20000 -hidden 0 > result/h_00_ep_20000_w2v
-python training.py -train opend_resverse -epoch 20000 -hidden 0 > result/h_00_ep_20000_w2v_reverse
+python training.py -train opened -epoch 20000 -hidden 0 > result/h_00_ep_20000_w2v
+python training.py -train opened_resverse -epoch 20000 -hidden 0 > result/h_00_ep_20000_w2v_reverse
 
 echo
 echo "h_01_ep_w2v"
 echo
 #python training.py -id origin_scale -w2v 1 -epoch 10000 -hidden 1 > result/reverse_h_01_ep_10000_w2v
-python training.py -train opend -epoch 10000 -hidden 1 > result/h_01_ep_10000_w2v
-python training.py -train opend_resverse -epoch 10000 -hidden 1 > result/h_01_ep_10000_w2v_reverse
+python training.py -train opened -epoch 10000 -hidden 1 > result/h_01_ep_10000_w2v
+python training.py -train opened_resverse -epoch 10000 -hidden 1 > result/h_01_ep_10000_w2v_reverse
 
 echo
 echo "h_02_ep_w2v"
 echo
 #python training.py -id origin_scale -w2v 1 -epoch 10000 -hidden 2 > result/reverse_h_02_ep_10000_w2v
-python training.py -train opend -epoch 10000 -hidden 2 > result/h_02_ep_10000_w2v
-python training.py -train opend_resverse -epoch 5000 -hidden 2 > result/h_02_ep_5000_w2v_reverse
+python training.py -train opened -epoch 10000 -hidden 2 > result/h_02_ep_10000_w2v
+python training.py -train opened_resverse -epoch 5000 -hidden 2 > result/h_02_ep_5000_w2v_reverse
 
 
