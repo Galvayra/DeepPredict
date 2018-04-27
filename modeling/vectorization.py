74b51d25e01b55d9e32d722f75397f7d3d6760ce
Galvayra <voltexlicer@gmail.com>
2018-04-27 16:31:22 +0900

    modify vectorization script

--- modeling/vectorization.py
+++ modeling/vectorization.py
@@ -12,7 +12,7 @@ class MyVector:
         self.vector_list = list()
 
     def encoding(self):
-        def __init_vector_dict__():
+        def __init_vector_dict():
             vector_dict = OrderedDict()
             vector_dict["x_train"] = x_train
             vector_dict["y_train"] = y_train
@@ -21,7 +21,7 @@ class MyVector:
 
             return vector_dict
 
-        def __set_x_data_dict__(is_manual=False, is_test=False):
+        def __set_x_data_dict(is_manual=False, is_test=False):
             x_dict = dict()
 
             if is_manual:
@@ -57,16 +57,16 @@ class MyVector:
                 for i in range(op.NUM_FOLDS):
                     y_train = y_data[:i * subset_size] + y_data[(i + 1) * subset_size:]
                     y_test = y_data[:i * subset_size] + y_data[(i + 1) * subset_size:]
-                    x_train = my_encoder.fit(__set_x_data_dict__(), len(y_train))
-                    x_test = my_encoder.fit(__set_x_data_dict__(), len(y_test))
-                    self.vector_list.append(__init_vector_dict__())
+                    x_train = my_encoder.fit(__set_x_data_dict(), len(y_train))
+                    x_test = my_encoder.fit(__set_x_data_dict(), len(y_test))
+                    self.vector_list.append(__init_vector_dict())
             else:
                 for i in range(op.NUM_FOLDS):
                     y_train = y_data[:i * subset_size] + y_data[(i + 1) * subset_size:]
                     y_test = y_data[i * subset_size:][:subset_size]
-                    x_train = my_encoder.fit(__set_x_data_dict__(), len(y_train))
-                    x_test = my_encoder.fit(__set_x_data_dict__(is_test=True), len(y_test))
-                    self.vector_list.append(__init_vector_dict__())
+                    x_train = my_encoder.fit(__set_x_data_dict(), len(y_train))
+                    x_test = my_encoder.fit(__set_x_data_dict(is_test=True), len(y_test))
+                    self.vector_list.append(__init_vector_dict())
 
         # one fold
         else:
@@ -74,15 +74,15 @@ class MyVector:
 
             y_train = y_data[subset_size:]
             y_test = y_data[:subset_size]
-            x_train = my_encoder.fit(__set_x_data_dict__(is_manual=True), len(y_train))
-            x_test = my_encoder.fit(__set_x_data_dict__(is_manual=True, is_test=True), len(y_test))
-            self.vector_list.append(__init_vector_dict__())
+            x_train = my_encoder.fit(__set_x_data_dict(is_manual=True), len(y_train))
+            x_test = my_encoder.fit(__set_x_data_dict(is_manual=True, is_test=True), len(y_test))
+            self.vector_list.append(__init_vector_dict())
 
         del self.my_data
 
     def dump(self, do_show=True):
 
-        def __counting_mortality__(_data):
+        def __counting_mortality(_data):
             count = 0
             for _d in _data:
                 if _d == [1]:
@@ -115,7 +115,7 @@ class MyVector:
                 print()
                 print("\nData Set", i+1)
                 print("Train total count -", str(len(self.vector_list[i]["x_train"]["merge"])).rjust(4),
-                      "\tmortality count -", str(__counting_mortality__(self.vector_list[i]["y_train"])).rjust(4))
+                      "\tmortality count -", str(__counting_mortality(self.vector_list[i]["y_train"])).rjust(4))
                 print("Test  total count -", str(len(self.vector_list[i]["x_test"]["merge"])).rjust(4),
-                      "\tmortality count -", str(__counting_mortality__(self.vector_list[i]["y_test"])).rjust(4))
+                      "\tmortality count -", str(__counting_mortality(self.vector_list[i]["y_test"])).rjust(4))
             print()
