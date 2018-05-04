a6ba3c7554c98e744dd4d0856a04822243fd8acd
Galvayra <voltexlicer@gmail.com>
2018-05-04 19:21:09 +0900

    erase import plot in train.py

--- learning/train.py
+++ learning/train.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import DeepPredict.arguments as op
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
